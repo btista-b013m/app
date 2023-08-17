029a6df56f911fac6321f626a3d131a98cb71a76
btista-b013m <btista-b013m@kinal.edu.gt>
2023-08-16 20:27:42 -0600

    Inicializando a subir app

---
+++ carrera-tecnica.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarreraTecnicaComponent } from './components/carrera-tecnica/carrera-tecnica.component';
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormCarreraTecnicaComponent } from './components/carrera-tecnica/form-carrera-tecnica.component';
+//import { AspiranteModule } from '../aspirante/aspirante.module';
+//import { AlumnoModule } from '../alumno/alumno.module';
+
+
+
+@NgModule({
+  declarations: [
+    CarreraTecnicaComponent,
+    FormCarreraTecnicaComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
+    //AspiranteModule,
+    //AlumnoModule
+  ]
+})
+export class CarreraTecnicaModule { }
+
